24bc4642f72ccec49ff935a207d424c86bd7e8b9
SignpostMarv <me@signpostmarv.name>
2024-12-04 00:18:56 +0000

    adding 1.0.1.1

--- src/AssetHttp.cs
+++ src/AssetHttp.cs
@@ -22,6 +22,7 @@ class AssetHttp
 			new Games.Satisfactory("0.8.3.3", EGame.GAME_UE5_2),
 			new Games.Satisfactory("1.0.0.7", EGame.GAME_UE5_3),
 			new Games.Satisfactory("1.0.1.0", EGame.GAME_UE5_3),
+			new Games.Satisfactory("1.0.1.1", EGame.GAME_UE5_3),
 		};
 
 		foreach (Games.Satisfactory version in versions)
