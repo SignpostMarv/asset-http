7add27efce151c3623dd04178497d1840732b92f
SignpostMarv <me@signpostmarv.name>
2024-12-11 01:37:02 +0000

    remove hardcoded version data

--- src/AssetHttp.cs
+++ src/AssetHttp.cs
@@ -1,10 +1,20 @@
 ﻿using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
+using System.Text.Json.Nodes;
+using Json.Schema;
 using SkiaSharp;
 using CUE4Parse.UE4.Versions;
 
 namespace SatisfactorDotDev.AssetHttp;
+
+class AssetHttpJsonItem
+{
+	public string version;
+
+	public string unreal_engine;
+}
 
 class AssetHttp
 {
@@ -18,13 +28,80 @@ class AssetHttp
 
 		listener.Prefixes.Add($"http://localhost:5000/");
 
-		Games.Satisfactory[] versions = {
-			new Games.Satisfactory("0.8.3.3", EGame.GAME_UE5_2),
-			new Games.Satisfactory("1.0.0.7", EGame.GAME_UE5_3),
-			new Games.Satisfactory("1.0.1.0", EGame.GAME_UE5_3),
-			new Games.Satisfactory("1.0.1.1", EGame.GAME_UE5_3),
-			new Games.Satisfactory("1.0.1.2", EGame.GAME_UE5_3),
-		};
+		string schema_string = """
+			{
+				"$schema": "https://json-schema.org/draft/2020-12/schema",
+				"$defs": {
+					"unreal_engine": {
+						"type": "string",
+						"enum": [
+							"5.2",
+							"5.3"
+						]
+					},
+					"semver": {
+						"type": "string",
+						"pattern": "^\\d+\\.\\d+\\.\\d+\\.\\d+$"
+					}
+				},
+				"type": "array",
+				"minItems": 1,
+				"items": {
+					"type": "object",
+					"required": [
+						"version",
+						"unreal_engine"
+					],
+					"properties": {
+						"version": {
+							"$ref": "#/$defs/semver"
+						},
+						"unreal_engine": {
+							"$ref": "#/$defs/unreal_engine"
+						}
+					}
+				}
+			}
+
+		""";
+
+		JsonSchema schema = JsonSchema.FromText(schema_string);
+
+		string data_contents;
+
+		using (StreamReader stream = new StreamReader("./satisfactory.json", Encoding.UTF8)) {
+			data_contents = stream.ReadToEnd();
+		}
+
+		JsonNode data = JsonNode.Parse(data_contents);
+
+		EvaluationResults data_items = schema.Evaluate(data);
+
+		if ( ! data_items.IsValid) {
+			throw new Exception("data not valid!");
+		}
+
+		List<Games.Satisfactory> versions = new List<Games.Satisfactory>();
+
+		foreach (JsonObject entry in (JsonArray) data) {
+			EGame unreal_engine;
+
+			switch (entry["unreal_engine"].ToString()) {
+				case "5.2":
+					unreal_engine = EGame.GAME_UE5_2;
+					break;
+				case "5.3":
+					unreal_engine = EGame.GAME_UE5_3;
+					break;
+				default:
+					throw new Exception("Unsupported Unreal Engine version");
+			}
+
+			versions.Add(new Games.Satisfactory(
+				entry["version"].ToString(),
+				unreal_engine
+			));
+		}
 
 		foreach (Games.Satisfactory version in versions)
 		{
